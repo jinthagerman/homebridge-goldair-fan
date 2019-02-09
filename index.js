7e3136447d023548eba65c38b6a2ee01f7b7fdc3
Jin <gbudelmann@gmail.com>
2019-02-09 22:27:54 +1300

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  
+  homebridge.registerAccessory("homebridge-goldair-fan", "Goldair SleepSmart", FanAccessory);
+}
+
+function FanAccessory(log, config) {
+  this.log = log;
+  this.name = config["name"];
+  this.accessToken = config["id"];
+  this.lockID = config["key"];
+  
+  this.service = new Service.Fanv2(this.name);
+
+  //this.addCharacteristic(Characteristic.Active);
+  //this.addOptionalCharacteristic(Characteristic.CurrentFanState);
+  //this.addOptionalCharacteristic(Characteristic.TargetFanState);
+  //this.addOptionalCharacteristic(Characteristic.LockPhysicalControls);
+  //this.addOptionalCharacteristic(Characteristic.Name);
+  //this.addOptionalCharacteristic(Characteristic.RotationDirection);
+  //this.addOptionalCharacteristic(Characteristic.RotationSpeed);
+  //this.addOptionalCharacteristic(Characteristic.SwingMode);
+
+  this.service
+    .getCharacteristic(Characteristic.Active)
+    .on('get', this.getState.bind(this));
+    .on('set', this.setState.bind(this));
+
+  this.service
+    .getCharacteristic(Characteristic.RotationSpeed)
+    .on('get', this.getState.bind(this))
+    .on('set', this.setState.bind(this));
+
+  this.service
+    .getCharacteristic(Characteristic.SwingMode)
+    .on('get', this.getState.bind(this))
+    .on('set', this.setState.bind(this));
+
+  this.service
+    .getCharacteristic(Characteristic.LockPhysicalControls)
+    .on('get', this.getState.bind(this))
+    .on('set', this.setState.bind(this));
+}
+
+LockitronAccessory.prototype.getState = function(callback) {
+  this.log("Getting current state...");
+}
+  
+LockitronAccessory.prototype.setState = function(state, callback) {
+  this.log("Set state to %s", lockitronState);
+}
+
+LockitronAccessory.prototype.getServices = function() {
+  return [this.service];
+}
